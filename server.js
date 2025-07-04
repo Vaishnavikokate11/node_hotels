460588b48dd1fbee27cf8c4692054332b7393f6e
Vaishnavikokate11 <vaishnavikokate55@gmail.com>
2025-07-04 17:06:13 +0530

    add port

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ const db = require('./db');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
-
+const PORT = process.env.PORT || 3000
 
 
 app.get('/',function (req, res){
